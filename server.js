b25e6841e2e273a52ed595ba54e4e3958cf3e354
milestarricone <milestarricone2@gmail.com>
2024-11-22 16:38:09 -0500

    skeleton done. all routes created.

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const morgan = require(`morgan`)
 const app = express()
 const PORT = 3001
 const Pet = require(`./models/pet.js`)
+const { render } = require("ejs")
 
 app.use(express.urlencoded({ extended: false}))
 app.use(methodOveride(`_method`))
@@ -23,18 +24,65 @@ mongoose.connection.on(`connected`, () => {
 })
 //Boilerplate above
 ///++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-app.get(`/`, async (req, res) => {
+
+
+///==Testing variables for routes
+let home = `/`
+let petsPage = `/pets`
+
+
+///==Testing variables for routes
+
+
+
+///home page
+app.get(home, async (req, res) => {
     console.log(`pets home page`);
     res.render(`index.ejs`)
 })
+///pets page
+app.get(petsPage, async (req, res) => { ////remember to give the pets page access to the database.
+    const allPets = await Pet.find({})
+    res.render(`pets/index.ejs`, {pets: allPets})
+    console.log(`pets all page`);
+    
+})
+///add pet page
+app.get(`/pets/addPet`, async (req, res) => {
+    // res.send(`add pet page`)
+    res.render(`pets/addPet.ejs`)
+})
+//creating a pet function
+app.post(`/pets`, async (req, res) => {/// redirect back to pets page after adding. 
+    console.log(req.body)
+    await Pet.create(req.body)
+    res.redirect(`/pets`)
+    
+})
 
+//show pet page
+app.get(`/pets/:petId`, async (req, res) => {
+    const foundPet = await Pet.findById(req.params.petId)
+    res.render(`pets/show.ejs`, {pet: foundPet})
+})
+//edit pet page==================================
+app.get(`/pets/:petId/updatePet`, async (req, res) => {
+    const foundPet = await Pet.findById(req.params.petId)
+    console.log(foundPet);
+    res.render(`pets/updatePet.ejs`, {
+        pet: foundPet,
+    })
 
-
-
-
-
-
-
+    // res.send(`edit the pet`)
+})
+app.put(`/pets/:petId`, async (req, res) => {
+    await Pet.findByIdAndUpdate(req.params.petId, req.body)
+    res.redirect(`/pets/${req.params.petId}`)
+})
+app.delete(`/pets/:petId`, async (req, res) => {
+    await Pet.findByIdAndDelete(req.params.petId)
+    res.redirect(`/pets`)
+})
 
 
 
@@ -45,3 +93,4 @@ app.listen(PORT, () => {
     console.log(`Listening on Port: ${PORT}.`);
     
 })
+// console.log(`test`);
